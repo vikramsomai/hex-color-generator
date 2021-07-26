ff89c80cbabfc98eb4435eaa1624cf58638f4744
vikram <vikramsomai4@gmail.com>
2021-07-26 11:02:10 +0530

    vbb# Please enter the commit message for your changes. Lines starting

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn = document.querySelector(".btn");
+const color = document.querySelector(".color");
+const copycolor = document.getElementById("copy-color");
+copycolor.addEventListener('click', function()
+{
+    color.select();
+    document.execCommand("copy");
+    alert("Copied the text: " + copy.value);
+});
+btn.addEventListener('click', function(){
+   let hexcolor='#';
+   for(let i=0;i<6;i++)
+   {
+  hexcolor +=hex[getnumber()];
+   }
+   color.innerHTML=hexcolor;
+    document.body.style.backgroundColor=hexcolor;
+});
+function getnumber()
+{
+    return Math.floor(Math.random() * hex.length)
+    
+}
