b16dab15253ed551d2a5079c8edaf4e4301fcee8
vikram somai <vikramsomai4@gmail.com>
2025-05-27 09:37:00 +0530

    Update new ui

--- src/app.js
+++ src/app.js
@@ -2,17 +2,15 @@ const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 const btn = document.querySelector(".btn");
 const color = document.querySelector(".color");
 
-btn.addEventListener('click', function(){
-   let hexcolor='#';
-   for(let i=0;i<6;i++)
-   {
-  hexcolor +=hex[getnumber()];
-   }
-   color.innerHTML=hexcolor;
-    document.body.style.backgroundColor=hexcolor;
+btn.addEventListener("click", function () {
+  let hexcolor = "#";
+  for (let i = 0; i < 6; i++) {
+    hexcolor += hex[getnumber()];
+  }
+  color.innerHTML = hexcolor;
+  document.body.style.backgroundColor = hexcolor;
 });
-function getnumber()
-{
-    return Math.floor(Math.random() * hex.length)
-    
-}
+
+const getnumber = () => {
+  return Math.floor(Math.random() * hex.length);
+};
